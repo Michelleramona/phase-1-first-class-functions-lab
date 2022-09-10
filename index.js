bf91475aea7dc4d95c6b681c622a3b0876c740e4
Michelle Vitello <michelleramona89@gmail.com>
2022-09-10 14:03:34 -0400

    commit tree 5074fc2846897bb2502560eaa8a965a8a569c083

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arrayOfDrivers) {
+    return [arrayOfDrivers[0],arrayOfDrivers[1]]
+  };
+
+  const returnLastTwoDrivers = function(arrayOfDrivers) {
+    return [arrayOfDrivers[2],arrayOfDrivers[3]]
+  };
+
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+  function createFareMultiplier(n1){
+        return function multiply(n2) {
+            return n1*n2
+        }
+  }
+
+  const fareDoubler = createFareMultiplier(2);
+
+  const fareTripler = createFareMultiplier(3);
+
+//createFareMultiplier(2);
+
+function selectDifferentDrivers(arrayOfDrivers, cb){
+    return cb(arrayOfDrivers)
+}
+
+//selectDifferentDrivers(2,3, createFareMultiplier)
+  
